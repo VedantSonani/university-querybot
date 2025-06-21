a4624fee51f52ecff73e8dde9323ba6115a497b5
Vedant Sonani <131787282+VedantSonani@users.noreply.github.com>
2025-06-21 14:23:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+import training
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import tensorflow
+import tflearn
+import numpy
+import os
+import random
+import pickle
+import json
+
+intent_file = "intents_3.json"
+NO_OF_NEURONS = 20
+
+
+class chatbot:
+    def __init__(self):
+        with open("data.pickle", "rb") as file:
+            self.words, self.labels, self.training, self.output = pickle.load(file)
+
+        tensorflow.compat.v1.reset_default_graph()
+
+        net = tflearn.input_data(shape=[None, len(self.training[0])])
+        net = tflearn.fully_connected(net, NO_OF_NEURONS)
+        net = tflearn.fully_connected(net, NO_OF_NEURONS)
+        net = tflearn.fully_connected(net, len(self.output[0]), activation="softmax")
+        net = tflearn.regression(net)
+
+        self.model = tflearn.DNN(net)
+
+        try:
+            self.model.load("models/model.tflearn")
+        except:
+            print("Model not present, train the model first...")
+            exit()
+
+    def input_encoded(self, user_input, words):
+        bag = [0 for i in range(len(words))]
+
+        user_input = nltk.word_tokenize(user_input)
+        user_input = [stemmer.stem(w.lower()) for w in user_input]
+
+        for word in user_input:
+            if word in words:
+                bag[words.index(word)] = 1
+        
+        return numpy.array(bag)
+
+    def chat(self):
+        os.system('cls')
+        print("starting...")
+        while True:
+            user_input = input("You: ")
+
+            if user_input.lower() == "quit":
+                break
+        
+            result = self.model.predict([self.input_encoded(user_input, self.words)])[0]
+            result_index = numpy.argmax(result)
+
+
+            if result[result_index] >= 0.7:
+                tag = self.labels[result_index]
+
+                with open(intent_file) as file:
+                    data = json.load(file) 
+
+                for t in data["intents"]:
+                    if t["tag"] == tag:
+                        responses = t["responses"]
+                        break
+                
+                output = random.choice(responses)
+                print("Bot: ", output)
+                print("percent: ", result[result_index])
+
+            else:
+                print("I didn't understand that. Please ask another question.")
+                print("percent: ", result[result_index])
+
+if __name__=="__main__":
+    obj = chatbot()
+    obj.chat()
